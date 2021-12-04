9d30821d95032302e7e33dac50f5d836c5a4f6ad
Rosario de Sousa Fondo <rosarioogenio@gmail.com>
2021-12-04 12:43:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ def get_crypto_price(coin):
     #text = soup.find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
 	
 	return string
+
+#Create a main function to consistently show the price of the cryptocurrency
+def main():
+  #Set the last price to negative one
+  last_price = -1
+  #Create an infinite loop to continuously show the price
+  while True:
+    #Choose the cryptocurrency that you want to get the price of (e.g. bitcoin, litecoin)
+    crypto = input("Please, enter the cryptocoin: ")
+    #Get the price of the crypto currency
+    price = get_crypto_price(crypto)
+    #Check if the price changed
+    if price != last_price:
+      print(crypto+' price: ',price) #Print the price
+      last_price = price #Update the last price
+    time.sleep(3) #Suspend execution for 3 seconds.
+
+main()
